3ffd1cfd00ea3798443981fa8b969b9fae9f5dc4
Adrian Harabula <adrian.harabula@gmail.com>
2017-09-21 11:28:37 +0200

    update log handling

--- loop_serve.sh
+++ loop_serve.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Run with
-# bash loop_serve.sh IP PORT logfilename.txt
+# bash loop_serve.sh IP PORT socketserver.log >> respawn_loop.log &
 
 # Or add the following line to crontab
-# @reboot cd ~/web/commmanager/modules/cop_module/socket-server && bash loop_serve.sh 0.0.0.0 13001 socketserverlog.txt >> loop_errors.txt &
+# @reboot cd /path/to/socketserver && bash loop_serve.sh 0.0.0.0 13001 socketserver.log >> logs/respawn_loop.log &
 
-until php server.php $1 $2 >> $3; do
+until php server.php $1 $2 >> logs/socketserver_loop/$3; do
     SERVER_STATUS=$?
     echo "[`date`] Server crashed with exit code $SERVER_STATUS. Respawning.."
     sleep 1;
